7d48167d57995661f4758897da9bb814e718dfd9
Ryan <rcmaddox@gmail.com>
2025-09-19 10:19:13 -0500

    single page with gallery

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,13 @@
 module.exports = function(eleventyConfig) {
+    // Passthrough copies for static assets and node_modules
     eleventyConfig.addPassthroughCopy("node_modules");
+    eleventyConfig.addPassthroughCopy("src/images");
+    eleventyConfig.addPassthroughCopy("src/admin");
+
+    // Explicitly add the gallery collection
+    eleventyConfig.addCollection("gallery", function(collectionApi) {
+        return collectionApi.getFilteredByGlob("src/gallery/*.md");
+    });
 
     return {
         dir: {
